3740fd2bff5ce0099e9d08e28b2f1fd975a5e48c
Anshul Modi <anshulmodi94@gmail.com>
2025-12-01 20:04:02 +0530

    program on readability level.

---
+++ week2/readability.c
@@ -0,0 +1,67 @@
+// program to determine the reading grade level of the text provided by the user
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+// prototypes
+float readingLevel(char *reading);
+
+int main(void)
+{
+    char reading[1000];
+
+    printf("Enter the text:\n");
+    fgets(reading, sizeof(reading), stdin);
+
+    float level = readingLevel(reading);
+    if(level < 1)
+    {
+        printf("Below level 1.\n");
+    }
+    else
+    {
+        printf("Level %.0f.\n", round(level));
+    }
+
+    return 0;
+}
+
+// calculating reading level; choosing not to abstract it across multiple functions, since I can use the same loop
+float readingLevel(char *reading)
+{
+    int index = 0;
+    float space = 0;
+    float letter = 0;
+    float period = 0;
+
+    // looping until it hits the end of the input sting
+    for(index = 0; index < strlen(reading); index++)
+    {
+        // running a condition to count the value occurances and using them to calculate the level
+        if(reading[index] == ' ')
+        {
+            // indicates the number of words e.g., a sentence with 4 words will have 3 spaces, so on and so forth
+            space++;
+        }
+        else if(isalpha(reading[index]))
+        {
+            // indicates all alphabets present
+            letter++;
+        }
+        else if(reading[index] == '.' || reading[index] == '!' || reading[index] == '?')
+        {
+            // indicates the number of sentences based on number of sentence terminations
+            period++;
+        }
+    }
+    
+    // calculating the components of the equation
+    float averageLetters = (letter / (space + 1)) * 100;
+    float averageSentences = (period / (space + 1)) * 100;
+    
+    float level = 0.0588 * averageLetters - 0.296 * averageSentences - 15.8;
+
+    return level;
+}
