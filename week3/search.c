f37a54aaf991eb6f2e3014fb9ec3933d162ed96b
Anshul Modi <anshulmodi94@gmail.com>
2025-12-08 15:52:07 +0530

    algorithms week

---
+++ week3/search.c
@@ -0,0 +1,22 @@
+// program to run a search in the array of integers provided compared to the user input
+
+#include <stdio.h>
+
+int main(void)
+{
+    int user_input = 0;
+    int array[] = {12, 53, 3, 234, 62, 56, 216, 353};
+    printf("Enter a number:\n");
+    scanf("%d", &user_input);
+
+    for(int index = 0; index < sizeof(array); index++)
+    {
+        if(user_input == array[index])
+        {
+            printf("Found.\n");
+            return 0;
+        }
+    }
+    printf("Not found.\n");
+    return 1;
+}
