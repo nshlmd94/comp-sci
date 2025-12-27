5d5fcf1c665a5a412faa77c4b90cfbdc2fd56ec8
Anshul Modi <anshulmodi94@gmail.com>
2025-12-27 13:29:02 +0530

    finished tideman

--- week3/tideman.c
+++ week3/tideman.c
@@ -40,8 +40,9 @@ bool vote(int rank, string candidateName, int ranks[]);
 void recordPreferences(int ranks[]);
 void addPairs(void);
 void sortPairs(void);
-//void lockPairs(void);
-//void printWinner(void);
+void lockPairs(void);
+void printWinner(void);
+bool checkCycle(int winner, int loser);
 
 int main(int argc, char* argv[])
 {
@@ -71,7 +72,7 @@ int main(int argc, char* argv[])
     {
         for(int j = 0; j < candidateCount; j++)
         {
-            locked[i][j] = 0;
+            locked[i][j] = 0; // setting the locked graph to zero
             preferences[i][j] = 0;
         }
     }
@@ -101,28 +102,16 @@ int main(int argc, char* argv[])
             }
         }
         printf("\n");
-        recordPreferences(ranks);
+        recordPreferences(ranks); // creates a 2D array (matrix) of the voter preferences
     }
-     
-    // CORRECT TILL HERE!!!! LET THE GAMES BEGIN!!!!!!!
 
+    // calling all relevant helper functions
     addPairs();
-
-    //debugging
-    for(int i = 0; i < pairCount; i++)
-    {
-        printf("Pair %d: winner = %d, loser = %d \n", i, pairs[i].winner, pairs[i].loser);
-    }
-    return 0;
-
-
     sortPairs();
-    
-    //debugging
-    for(int i = 0; i < pairCount; i++)
-    {
-        printf("Pair %d: winner = %d, loser = %d \n", i, pairs[i].winner, pairs[i].loser);
-    }
+    lockPairs();
+
+    printWinner();
+
     return 0;
 }
 
@@ -174,49 +163,117 @@ void addPairs(void)
                 pairs[pairCount].loser = j;
                 pairCount++;
             }
-            else if(preferences[i][j] < preferences[j][i])
-            {
-                pairs[pairCount].winner = j;
-                pairs[pairCount].loser = i;
-                pairCount++;
-            }
         }
     }
 }
 
-// my implementation of selection sort to sort the pairs in descending order
+// implementation of 'selection sort' to sort the pairs in descending order
 void sortPairs(void)
 {
-    int maxWinner = pairs[0].winner;
-    int maxLoser = pairs[0].loser;
+    int strength[pairCount];
+    
+    for(int index = 0; index < pairCount; index++)
+    {
+        strength[index] = preferences[pairs[index].winner][pairs[index].loser];
+    }
 
     for(int loopIndex = 0; loopIndex < pairCount-1; loopIndex++)
     {
+        int maxValue = strength[loopIndex];
         int tempIndex = loopIndex;
         for(int elementIndex = loopIndex+1; elementIndex < pairCount; elementIndex++)
         {
-            if(maxWinner < pairs[elementIndex].winner)
+            if(maxValue < strength[elementIndex])
             {
-                maxWinner = pairs[elementIndex].winner;
-                maxLoser = pairs[elementIndex].loser;
+                maxValue = strength[elementIndex];
                 tempIndex = elementIndex;
             }
         }
-        pairs[tempIndex].winner = pairs[loopIndex].winner;
-        pairs[tempIndex].loser = pairs[loopIndex].loser;
-        pairs[loopIndex].winner = maxWinner;
-        pairs[loopIndex].loser = maxLoser;
+
+        pair tempValue = pairs[tempIndex];
+        int tempStrength = strength[tempIndex];
+
+        pairs[tempIndex] = pairs[loopIndex];
+        strength[tempIndex] = strength[loopIndex];
+
+        pairs[loopIndex] = tempValue;
+        strength[loopIndex] = tempStrength;
     }
 }
 
-/*
+// locking in the pairs while checking there is no cycle created
 void lockPairs(void)
 {
+    for(int pairIndex = 0; pairIndex < pairCount; pairIndex++)
+    {
+        int winner = pairs[pairIndex].winner;
+        int loser = pairs[pairIndex].loser;
 
+        if(checkCycle(winner, loser) == false)
+        {
+            locked[winner][loser] = true;
+        }
+    }
 }
 
+// printing the winner of the election by checking which candidate is the source i.e., doesn't have any arrows pointing to it (no presence in j)
 void printWinner(void)
 {
+    int isNotSource[candidateCount];
 
+    // initialization of the array to 0
+    for(int i = 0; i < candidateCount; i++)
+    {
+        isNotSource[i] = 0;
+    }
+
+    // counting which candidates have a presence in j i.e., have arrows pointing towards them
+    for(int i = 0; i < candidateCount; i++)
+    {
+        for(int j = 0; j < candidateCount; j++)
+        {
+            if(locked[i][j] == 1)
+            {
+                isNotSource[j]++;
+            }
+        }
+    }
+
+    // if the isNotSource is 0, then the candidates has no arrows pointing towards it, hence they are declared the winner
+    for(int index = 0; index < candidateCount; index++)
+    {
+        if(isNotSource[index] == 0)
+        {
+            printf("The winner is: %s\n", candidates[index]);
+        }
+    }
 }
-*/
+
+// helper function to the lockPairs; takes in locked graph and and checks it for cycle via a base case and recursion
+bool checkCycle(int winner, int loser)
+{
+    bool cycleStatus = false; // sets the cycle status to false
+
+    // base case: checks if the loser points towards the winner -> if it does, then returns true, because it creates a cycle
+    if(locked[loser][winner] == true)
+    {
+        cycleStatus = true;
+        return cycleStatus;
+    }
+
+    // if the base condition is false, then it check if the loser points towards another candidate, if it does, then it calls the checkCycle recursively to see if it points to the winner
+    for(int candidateIndex = 0; candidateIndex < candidateCount; candidateIndex++)
+    {
+        if(locked[loser][candidateIndex] == true)
+        {
+            cycleStatus = checkCycle(winner, candidateIndex);
+            // if true is returned i.e., a cycle is found, then exit early
+            if(cycleStatus == true)
+            {
+                return cycleStatus;
+            }
+        }
+    }
+
+    return cycleStatus; // returns the status of the cycle
+}
